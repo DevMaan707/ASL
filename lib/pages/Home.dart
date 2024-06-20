a0d3ef66cb099b89f4d97361b5618a589a807f12
DevMaan707 <dev.maan707@gmail.com>
2024-06-21 02:12:54 +0530

    added home screen

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,3 +1,5 @@
+import 'package:aslcom/utils/customs/MySIzes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,10 +10,73 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String indicator = "keyboard_voice_rounded";
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    TextEditingController emailController = TextEditingController();
     return Scaffold(
-      body: Center(child: Text("Home"),),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: screenWidth,
+                    height: screenHeight * 0.85,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: screenWidth * 0.7,
+                        child: TextFormField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            hintText: "Username",
+                            prefixIcon: Icon(
+                              CupertinoIcons.volume_down,
+                              size: MySizes(context).standardPadding,
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.keyboard_voice_rounded,
+                          color: Colors.red,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: (indicator == "keyboard_voice_rounded")
+                  ? IconButton(
+                      onPressed: () {
+                        setState(() {
+                          indicator = "keyboard_voice_rounded";
+                        });
+                      },
+                      icon: Icon(Icons.keyboard_voice_rounded))
+                  : IconButton(onPressed: () {
+                    setState(() {
+                      indicator = "";
+                    });
+              }, icon: Icon(Icons.camera),),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
