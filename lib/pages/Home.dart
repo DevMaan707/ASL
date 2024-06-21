ef0926f8922c5d0b63a4465883d6f1d6f2a89bb4
DevMaan707 <dev.maan707@gmail.com>
2024-06-22 00:00:33 +0530

    added video player and microphone functionality

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,6 +1,8 @@
+import 'package:aslcom/pages/VideoPlayer.dart';
 import 'package:aslcom/utils/customs/MySIzes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,12 +12,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String wordsSpoken = "";
   String indicator = "keyboard_voice_rounded";
+  TextEditingController textController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    initSpeech();
+  }
+
+  void initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+  }
+
+  void _onSpeechResult(result) {
+    setState(() {
+      wordsSpoken = "${result.recognizedWords}";
+      textController.text = wordsSpoken;
+      print(wordsSpoken);
+    });
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    TextEditingController emailController = TextEditingController();
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
@@ -26,9 +60,10 @@ class _HomeState extends State<Home> {
                   Container(
                     width: screenWidth,
                     height: screenHeight * 0.85,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Colors.blue,
                     ),
+                    child: const VideoPlayerWidget(anim:["assets/videos/idle.mp4"]),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -36,9 +71,9 @@ class _HomeState extends State<Home> {
                       SizedBox(
                         width: screenWidth * 0.7,
                         child: TextFormField(
-                          controller: emailController,
+                          controller: textController,
                           decoration: InputDecoration(
-                            hintText: "Username",
+                            hintText: "Edit the text..",
                             prefixIcon: Icon(
                               CupertinoIcons.volume_down,
                               size: MySizes(context).standardPadding,
@@ -47,10 +82,11 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                       IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          _speechToText.isListening ? _stopListening : _startListening;
+                        },
                         icon: Icon(
-                          Icons.keyboard_voice_rounded,
-                          color: Colors.red,
+                            _speechToText.isListening ? Icons.stop : Icons.mic
                         ),
                       )
                     ],
@@ -60,19 +96,22 @@ class _HomeState extends State<Home> {
             ),
             Align(
               alignment: Alignment.topRight,
-              child: (indicator == "keyboard_voice_rounded")
-                  ? IconButton(
-                      onPressed: () {
-                        setState(() {
-                          indicator = "keyboard_voice_rounded";
-                        });
-                      },
-                      icon: Icon(Icons.keyboard_voice_rounded))
-                  : IconButton(onPressed: () {
-                    setState(() {
-                      indicator = "";
-                    });
-              }, icon: Icon(Icons.camera),),
+              child: Padding(
+                padding: EdgeInsets.only(top:40),
+                child: (indicator == "keyboard_voice_rounded")
+                    ? IconButton(
+                        onPressed: () {
+                          setState(() {
+                            indicator = "keyboard_voice_rounded";
+                          });
+                        },
+                        icon: const Icon(Icons.keyboard_voice_rounded))
+                    : IconButton(onPressed: () {
+                      setState(() {
+                        indicator = "";
+                      });
+                }, icon: const Icon(Icons.camera),),
+              ),
             )
           ],
         ),
---
+++ lib/pages/VideoPlayer.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatefulWidget {
+  final List<String> anim;
+  const VideoPlayerWidget({super.key, required this.anim});
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  VideoPlayerController? _controller;
+  int _currentIndex = 0;
+  late List<String> _videos;
+
+  @override
+  void initState() {
+    super.initState();
+    _videos = widget.anim;
+    _initializeAndPlay(_videos[_currentIndex]);
+  }
+
+  Future<void> _initializeAndPlay(String videoPath) async {
+    _controller = VideoPlayerController.asset(videoPath)
+      ..addListener(() {
+        if (_controller!.value.position == _controller!.value.duration) {
+          _playNextVideo();
+        }
+      })
+      ..initialize().then((_) {
+        setState(() {});
+        _controller!.play();
+      });
+  }
+
+  void _playNextVideo() {
+    if (_currentIndex < _videos.length - 1) {
+      _currentIndex++;
+      _controller!.dispose();
+      _initializeAndPlay(_videos[_currentIndex]);
+    } else {
+      _currentIndex = 0;
+      _controller!.dispose();
+      _initializeAndPlay(_videos[_currentIndex]);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller != null && _controller!.value.isInitialized
+        ? AspectRatio(
+      aspectRatio: _controller!.value.aspectRatio,
+      child: VideoPlayer(_controller!),
+    )
+        : const Center(child: CircularProgressIndicator());
+  }
+}
